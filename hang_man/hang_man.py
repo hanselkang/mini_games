94ed0d3a2d23dd8c3473e141fdacbf8895b92ee7
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-06 21:47:48 +0100

    hangman reset

--- hang_man/hang_man.py
+++ hang_man/hang_man.py
@@ -1,94 +1,35 @@
-# Import sources
-# words list
-# random
-# time
-
-import time
 import random
-from IPython.display import clear_output
+import words
 
 
-def display_board(board):
-    clear_output()
-    print(board[0], board[1], board[2], board[3], board[4])
 
 
-hang_man_board = ['_ '] * 5
+# Words list 
 
-# First display
+# Random Words (with length of 6)
 
-name = input("Please write down your name: ")
-print(f"Hi,{name}. Let's play Hangman game.")
+# Display _ * 6
 
-time.sleep(1)
-print("Loading...")
-print()
-time.sleep(0.5)
+# turn * 8
 
-# Words list
+# input character or a 6 letters word
 
-words = ["dream", "right", "fight", "flood", "water", "melon"]
+    # if it's not 1 or 6// ask again
 
-# Load quiz
+    # 1 character
 
-# Make list random and select a word
-random.shuffle(words)
-select_word = random.choice(words)
+        # if it's a char in the [w][o][r][d]
+            # show the letter in the quiz
+            # if not, do nothing
+        # turn -= 1
 
-# Answer
-# In case import words, remove space in string
-word = select_word[0].strip()
+    # 6 letters word
 
-# Guess from player
-guess_word = ''
+        #random words != letters word
+            
+            #turn -=1
+            #back to input
 
-# Chances to play
+        #random words == letters word
 
-# Play game
-
-
-turns = 10
-chance = 10
-while turns > 0:
-    print(display_board(hang_man_board))
-    print("Chance =", turns)
-    print("Hints =", chance)
-    guess = input("Guess a character : ")
-    print(guess)
-    fail = 0  # if there is no match word
-    for char in word:
-        if char != guess_word:
-            print(f"{char} is right. Guess again.")
-            fail += 1
-
-        else:  # success
-            time.sleep(2)
-            print(".")
-            print(".")
-            print(".")
-            time.sleep(0.5)
-            print("Congratualtions! you are correct!")
-            break
-
-    # Hint
-    chances = 10
-    if chance >= chances:
-        time.sleep(2)
-        print(".")
-        print(".")
-        print(".")
-        time.sleep(0.5)
-        print('Hint : {}'.format(random.choice(select_word).strip()))
-        chances -= 1
-
-    
-
-    if guess not in word:
-        turns -= 1
-        print("Ooft, wrong guess!")
-        print(f"You have {turns} more chances.")
-
-        if turns == 0:
-            print()
-            print()
-            print("You failed. Hangman shutdown.")
+            # congratulations
